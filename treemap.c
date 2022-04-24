e5a34578f87650bef474f37fca612554da703093
Your Name <you@example.com>
2022-04-24 06:02:26 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -114,7 +114,7 @@ void removeNode(TreeMap * tree, TreeNode* node) {
         child->parent = parent;
         free(node);
     } else if(count == 2) {
-        TreeNode *bigger = maximum(node->right);
+        TreeNode *bigger = maximum(node);
         printf("root %d\n", *((int*)tree->root->pair->key));
         printf("\\_L %d\n", *((int*)tree->root->left->pair->key));
         printf("\\_R %d\n", *((int*)tree->root->right->pair->key));

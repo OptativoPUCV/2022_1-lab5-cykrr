57aee80233e39efaf1e8a773e26490bb846e1975
Your Name <you@example.com>
2022-04-24 07:07:44 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -189,7 +189,7 @@ Pair * nextTreeMap(TreeMap * tree) {
     if(tree->current->right) {
         tree->current = minimum(tree->current->right);
     } else {
-        while(tree->lower_than(tree->current->parent->pair->key, tree->current->pair->key))
+        while(tree->lower_than(tree->current->pair->key, tree->current->parent->pair->key))
             tree->current = tree->current->parent;
     }
 
